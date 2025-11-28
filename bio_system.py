fed4d4de6a66b77145284c48548585d17f073017
yononscc-g-025 <yononscc-g-025@student.wethinkcode.co.za>
2025-11-28 10:47:27 +0200

    added new lines

--- bio_system.py
+++ bio_system.py
@@ -1,15 +1,20 @@
 from datetime import datetime
 
 def id(ID):
-    with open("student_details.txt", "r") as f:
+    with open("student_details.txt", "r+") as f:
         next(f) # This skips the header line
         ID = input("Enter your ID: ")
         
-        for row in f:
-            users = row.strip().split() # This line removes an unwanted characters in the row and splits the remaining characters before putting them in a list
+        for column in f:
+            users = column.strip().split() # This line removes an unwanted characters in the row and splits the remaining characters before putting them in a list
             if users[0].strip() == str(ID).strip(): # The reason why we casted the ID into a string is because all the text in the text file are strings so we need to compare them using strings
                 time_entered = datetime.now().strftime("%H:%M:%S")
-                return f"Hi {users[1].strip()}. You have entered at {time_entered}"
+                time_in = time_entered
+                with open("gg.txt", "a") as f:
+                 
+                 f.write(f"\n{time_entered} {users}")
+                
+                 return f"Hi {users[1].strip()}. You have entered at {time_entered}"
             
         raise ValueError (f"User {ID} not found") # Raises ValueError if the ID entered by the user is not found 
 
